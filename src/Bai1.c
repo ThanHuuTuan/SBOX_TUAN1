90f7cd888b084304ea29782378f60417682961a9
Thân Hữu Tuấn <32167814+ThanHuuTuan@users.noreply.github.com>
2018-05-31 15:15:22 +0700

    Update Bai1.c

--- src/Bai1.c
+++ src/Bai1.c
@@ -5,9 +5,8 @@ int main() {
     int n, a, b, c, temp;
 
     printf("Nhap so nguyen n co 3 chu so): ");
-
     scanf("%d", &n);
-    if(n>=100 && n<=999){
+if(a>=100 && a<=999){
 
     a = n/100;
     b = ((n%100)/10);
@@ -34,7 +33,11 @@ int main() {
     printf("So can chuyen : %d%d%d", a, b, c);
     getch();
 
-}
+  }
 else
-    printf("Nhap dung so co 3 chu");
+  {
+      printf("Nhap so nguyen trong khoang 100-999");
+  }
+
+
 }
